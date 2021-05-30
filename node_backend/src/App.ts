ca67f3638132c62cd9e9566e47b897bdb60a26bc
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-30 22:57:49 +0530

    Update App.ts

--- node_backend/src/App.ts
+++ node_backend/src/App.ts
@@ -1,14 +1,39 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+require("dotenv/config");
 
 const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
 
-app.listen(port, (err: any) => {
-  if (err) {
-    return console.error(err);
+//Import routes
+const guestRoutes = require("./routes/GuestRoutes");
+const propertyRoutes = require("./routes/PropertyRoutes");
+const reservationRoutes = require("./routes/ReservationRoutes");
+const roomRateRoutes = require("./routes/RoomRateRoutes");
+const roomRoutes = require("./routes/RoomRoutes");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/guest", guestRoutes);
+app.use("/property", propertyRoutes);
+app.use("/reservation", reservationRoutes);
+app.use("/roomrate", roomRateRoutes);
+app.use("/room", roomRoutes);
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_CONNECTION_USER_ID}:${process.env.DB_CONNECTION_PASSWORD}@cluster0.nwd47.mongodb.net/HotelReservationDB?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("Connected to MonogoDB ");
   }
-  return console.log(`server is listening on ${port}`);
-});
+);
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
