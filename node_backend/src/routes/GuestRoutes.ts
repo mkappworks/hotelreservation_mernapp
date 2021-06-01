0cd94bf490c116571e938ef1d5015d56dc0e2a92
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-02 00:46:40 +0530

    updated GuestRoute

--- node_backend/src/routes/GuestRoutes.ts
+++ node_backend/src/routes/GuestRoutes.ts
@@ -1,49 +1,41 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import authenticateToken from "../middleware/AuthenticateToken";
+
 import Guest, { IGuest } from "../models/GuestModel";
 
 const router = express.Router();
 
 //router for adding a new guest
-router.post("", async (req, res) => {
-  const newRecord: IGuest = new Guest({
-    password: req.body.password,
-    name: req.body.name,
-    email: req.body.email,
-    mobileNumber: req.body.mobileNumber,
-  });
-
+router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newRecord: IGuest = new Guest({
+      email: req.body.email,
+      password: hashedPassword,
+      name: req.body.name,
+      mobileNumber: req.body.mobileNumber,
+    });
     const saveRecord = await newRecord.save();
     res.status(201).json(saveRecord);
     console.log("guest router post success");
   } catch (error) {
-    res.json({ message: error });
-  }
-});
-
-//router for getting all guest from the db
-router.get("", async (req, res) => {
-  try {
-    const collection: Array<IGuest> = await Guest.find();
-
-    res.status(200).json(collection);
-
-    console.log("guest router get success");
-  } catch (error) {
-    res.json({ message: error });
+    res.status(500).json({ message: error });
   }
 });
 
 //router for updating a guest with a specific id
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", authenticateToken, async (req, res) => {
   try {
     const updateRecord = await Guest.findByIdAndUpdate(
       { _id: req.params.id },
       {
         $set: {
+          email: req.body.email,
           password: req.body.password,
           name: req.body.name,
-          email: req.body.email,
           mobileNumber: req.body.mobileNumber,
         },
       }
@@ -58,7 +50,7 @@ router.patch("/:id", async (req, res) => {
 });
 
 //router for deleting a guest with a specific id
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", authenticateToken, async (req, res) => {
   try {
     const removeRecord = await Guest.findByIdAndRemove({
       _id: req.params.id,
@@ -67,6 +59,19 @@ router.delete("/:id", async (req, res) => {
     res.status(200).json(removeRecord);
 
     console.log("guest router delete success");
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//router for getting all guest from the db
+router.get("/getusers", authenticateToken, async (req, res) => {
+  try {
+    const collection: Array<IGuest> = await Guest.find();
+
+    res.status(200).json(collection);
+
+    console.log("guest router get success");
   } catch (error) {
     res.json({ message: error });
   }
