ed1c77bdfe0473ae1feee17506d2ef067eea41b8
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-02 00:46:56 +0530

    updated App.ts with AuthRoute

--- node_backend/src/App.ts
+++ node_backend/src/App.ts
@@ -1,25 +1,32 @@
 import express from "express";
-import cors from "cors";
+import Cors from "cors";
 import mongoose from "mongoose";
+// import passport from "passport";
+
 require("dotenv/config");
+//require("./config/passpart");
 
 const app = express();
+app.use(Cors());
+app.use(express.json());
+// app.use(passport.initialize());
 
 //Import routes
+const authRoutes = require("./routes/AuthRoutes");
 const guestRoutes = require("./routes/GuestRoutes");
 const propertyRoutes = require("./routes/PropertyRoutes");
 const reservationRoutes = require("./routes/ReservationRoutes");
 const roomRateRoutes = require("./routes/RoomRateRoutes");
 const roomRoutes = require("./routes/RoomRoutes");
 
-app.use(cors());
-app.use(express.json());
-
+app.use("/auth", authRoutes);
 app.use("/guest", guestRoutes);
 app.use("/property", propertyRoutes);
 app.use("/reservation", reservationRoutes);
 app.use("/roomrate", roomRateRoutes);
 app.use("/room", roomRoutes);
+
+process.env.ACCESS_TOKEN_SECRET;
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_CONNECTION_USER_ID}:${process.env.DB_CONNECTION_PASSWORD}@cluster0.nwd47.mongodb.net/HotelReservationDB?retryWrites=true&w=majority`,
